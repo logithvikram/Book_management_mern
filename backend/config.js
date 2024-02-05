50bfb67fc664c8cc31c9a395f13a933bbf04572f
logith vikram <106614043+logithvikram@users.noreply.github.com>
2024-02-05 20:54:07 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 3000;
-export const MONGODB_URI='mongodb+srv://logithvikram:9weYW3jRprEUNn3w@cluster0.miuoodu.mongodb.net/book'
+export const MONGODB_URI='mongodb+srv://logithvikram:<<<>>>@cluster0.miuoodu.mongodb.net/book'
 
 
  
